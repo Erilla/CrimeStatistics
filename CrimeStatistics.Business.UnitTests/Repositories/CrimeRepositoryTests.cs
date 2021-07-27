eed3031d416dc93c19d0533100d54320ef20119c
Ryan Wong <me@ryanwong.uk>
2021-07-27 02:45:22 +0100

    Implemented unit tests

---
+++ CrimeStatistics.Business.UnitTests/Repositories/CrimeRepositoryTests.cs
@@ -0,0 +1,112 @@
+﻿using CrimeStatistics.Business.Repositories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Moq.Protected;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CrimeStatistics.Business.UnitTests.Repositories
+{
+    [TestClass]
+    public class CrimeRepositoryTests
+    {
+        private CrimesRepository repository;
+
+        private Mock<IHttpClientFactory> httpClientFactoryMock;
+
+        [TestInitialize]
+        public void InitialiseTests()
+        {
+            httpClientFactoryMock = new Mock<IHttpClientFactory>();
+        }
+
+        [TestMethod]
+        public async Task GetCrimeCategories_SuccessResponseAsync()
+        {
+            var mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+            mockHttpMessageHandler.Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = new StringContent("[{}]"),
+                });
+
+            var client = new HttpClient(mockHttpMessageHandler.Object);
+            httpClientFactoryMock.Setup(_ => _.CreateClient(It.IsAny<string>())).Returns(client);
+
+            repository = new CrimesRepository(httpClientFactoryMock.Object);
+
+            var result = await repository.GetCrimeCategories();
+
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(HttpRequestException))]
+        public async Task GetCrimeCategories_ErrorResponseAsync()
+        {
+            var mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+            mockHttpMessageHandler.Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.InternalServerError,
+                    Content = new StringContent("[{}]"),
+                });
+
+            var client = new HttpClient(mockHttpMessageHandler.Object);
+            httpClientFactoryMock.Setup(_ => _.CreateClient(It.IsAny<string>())).Returns(client);
+
+            repository = new CrimesRepository(httpClientFactoryMock.Object);
+
+            var result = await repository.GetCrimeCategories();
+        }
+
+        [TestMethod]
+        public async Task GetCrimeStreet_SuccessResponseAsync()
+        {
+            var mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+            mockHttpMessageHandler.Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = new StringContent("[{}]"),
+                });
+
+            var client = new HttpClient(mockHttpMessageHandler.Object);
+            httpClientFactoryMock.Setup(_ => _.CreateClient(It.IsAny<string>())).Returns(client);
+
+            repository = new CrimesRepository(httpClientFactoryMock.Object);
+
+            var result = await repository.GetCrimeStreet(1, 2, DateTime.Now);
+
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(HttpRequestException))]
+        public async Task GetCrimeStreet_ErrorResponseAsync()
+        {
+            var mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+            mockHttpMessageHandler.Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.InternalServerError,
+                    Content = new StringContent("[{}]"),
+                });
+
+            var client = new HttpClient(mockHttpMessageHandler.Object);
+            httpClientFactoryMock.Setup(_ => _.CreateClient(It.IsAny<string>())).Returns(client);
+
+            repository = new CrimesRepository(httpClientFactoryMock.Object);
+
+            var result = await repository.GetCrimeStreet(1, 2, DateTime.Now);
+        }
+    }
+}
---
+++ CrimeStatistics.Business.UnitTests/StatisticsHandler/StatisticsHandlerTests.cs
@@ -0,0 +1,68 @@
+﻿using CrimeStatistics.Business.Models.PoliceApi;
+using CrimeStatistics.Business.Repositories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrimeStatistics.Business.UnitTests.StatisticsHandler
+{
+    [TestClass]
+    public class StatisticsHandlerTests
+    {
+        private Business.StatisticsHandler.StatisticsHandler handler;
+
+        private Mock<ICrimesRepository> crimeRepositoryMock;
+
+        [TestInitialize]
+        public void InitialiseTests()
+        {
+            crimeRepositoryMock = new Mock<ICrimesRepository>();
+        }
+
+        [TestMethod]
+        public async Task GetCrimeStatistics_SuccessfulResponseAsync()
+        {
+            var crimeStreetResponse = new CrimeStreetResponse
+            {
+                CrimeStreets = new List<CrimeStreet>
+                { 
+                    new CrimeStreet
+                    {
+                        category = "category",
+                        id = 123,
+                        month = "month"
+                    } 
+                }
+            };
+
+            crimeRepositoryMock
+                .Setup(cr => cr.GetCrimeStreet(It.IsAny<decimal>(), It.IsAny<decimal>(), It.IsAny<DateTime>()))
+                .ReturnsAsync(crimeStreetResponse);
+
+            var crimeCategoriesResponse = new CrimeCategoriesResponse
+            {
+                CrimeCategories = new List<CrimeCategory>
+                {
+                    new CrimeCategory
+                    {
+                        name = "CategoryName",
+                        url = "category"
+                    }
+                }
+            };
+
+            crimeRepositoryMock.Setup(cr => cr.GetCrimeCategories())
+                .ReturnsAsync(crimeCategoriesResponse);
+
+            handler = new Business.StatisticsHandler.StatisticsHandler(crimeRepositoryMock.Object);
+
+            var result = await handler.GetCrimeStatisticsAsync(1, 2, DateTime.Now);
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
